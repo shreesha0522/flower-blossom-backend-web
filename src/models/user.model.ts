458745353f3716d9fae530dff112a9ac0250522e
GAURAB <gaurabkhadka4082@gmail.com>
2026-01-04 13:32:25 +0545

    add username in user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,6 +5,7 @@ const UserSchema: Schema = new Schema<UserType>(
   {
     firstName: {type: String},
     lastName: {type: String},
+    username: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     role: {
